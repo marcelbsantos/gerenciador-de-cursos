520cfc9a862aaad6c96788d92076123cc5606a0f
marcel.bezerra <marcel.bezerra@arce.ce.gov.br>
2017-01-02 18:25:43 -0300

    classe novas

---
+++ src/br/com/alura/Curso.java
@@ -0,0 +1,45 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Curso {
+
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new ArrayList<>();
+	
+	
+	public Curso(String nome, String instrutor) {
+		super();
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+	
+	public String getInstrutor() {
+		return instrutor;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+	
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + " ,tempo Total: " + this.getTempoTotal() + " ," 
+		+ "aulas: " + this.aulas + "]";
+	}
+}
